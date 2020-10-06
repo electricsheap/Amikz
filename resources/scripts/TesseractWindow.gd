08d2bbc4cb8714bd266006f677aca3bbb799fbd3
Andrew J.C. Wilson <ajcheyneywilson@gmail.com>
2020-10-06 18:30:33 -0400

    began work on the HUD. created default theme

--- resources/scripts/TesseractWindow.gd
+++ resources/scripts/TesseractWindow.gd
@@ -1,7 +1,7 @@
 class_name TesseractWindow
 extends Polygon2D
 
-export var power: float = -1.0;
+export var power: float = 0.0;
 
 func _ready():
 	pass
@@ -9,6 +9,6 @@ func _ready():
 
 
 func _process( delta ):
-	power += delta/4
+#	power += delta/4
 	material.set_shader_param("global_transform", get_global_transform())
 	material.set_shader_param("power", self.power)
